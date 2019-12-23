a150acd6d7a745279ae43acb40936af870b1a27b
psykespb <psykespb@gmail.com>
2019-12-23 19:44:21 +0300

    added routing to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Menu from "./views/Menu";
+import Checkout from "./views/Checkout";
+import Test from "./views/Test";
 
 export default function App() {
   return (
-    <React.Fragment>
+    <Router>
       <CssBaseline />
-      <Menu></Menu>
-    </React.Fragment>
+      <Switch>
+        <Route exact path="/">
+          <Menu />
+        </Route>
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="/test">
+          <Test />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
