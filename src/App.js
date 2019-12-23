fe3579711e025d5dafd57f61a2c415826488a28a
psykespb <psykespb@gmail.com>
2019-12-23 19:48:00 +0300

    fixed app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Menu from "./views/Menu";
 import Checkout from "./views/Checkout";
-import Test from "./views/Test";
 
 export default function App() {
   return (
@@ -15,9 +14,6 @@ export default function App() {
         </Route>
         <Route exact path="/checkout">
           <Checkout />
-        </Route>
-        <Route exact path="/test">
-          <Test />
         </Route>
       </Switch>
     </Router>
